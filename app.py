2d4d707b5558bb05983f39675ee0a4f223292f90
AI-Scout <131296521+AI-Scout@users.noreply.github.com>
2023-09-11 21:26:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+from langchain.llms import OpenAI
 
 openai_api_key = st.secrets["OPEN_AI_API_KEY"]
 
