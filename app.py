9a92193ca0b3fa21af2d0827bc40debc9f2a9983
AI-Scout <131296521+AI-Scout@users.noreply.github.com>
2023-09-12 11:37:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -106,9 +106,10 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            </style>
             #GithubIcon {
               visibility: hidden;
             }
+            </style>
+
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
